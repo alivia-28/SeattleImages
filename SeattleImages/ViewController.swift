865d451ffa0201bc707df10806b4ecaa3d19e515
Alivia <withalivia@gmail.com>
2021-09-16 02:40:22 -0700

    Checking in code for Seattle image app which shows random images of Seattle

--- SeattleImages/ViewController.swift
+++ SeattleImages/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showMeNextRandomPicture(_ sender: Any) {
+        let num = Int.random(in: 1...10)
+        let imgName = "Seattle\(num)" // "Seattle" + String(num)
+        imgView.image = UIImage(named: imgName)
+    }
+    
 
 }
 
